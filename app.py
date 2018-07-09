68e9d50a9783eb1e8f24a49c55060c11853d6a71
juliennuellas <julien.nuellas@gmail.com>
2018-07-09 09:55:08 +0200

    First mock test

---
+++ app.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+# coding: utf-8
+import urllib.parse
+import urllib.request
+import urllib.error
+import json
+
+class OpenFoodFactsAPI:
+
+    def get_product_from_api(self, brand):
+
+        payload = {
+            'action' : 'process',
+            'json' : '1',
+            'tagtype_0' : 'brands',
+            'tag_contains_0' : 'contains',
+            'tag_0' : brand,
+            'page_size' : '150',
+            'page' : '1'
+        }
+
+        parameters = urllib.parse.urlencode(payload)
+        url = "http://fr.openfoodfacts.org/cgi/search.pl"
+        parameters = parameters.encode('utf-8')
+        req = urllib.request.Request(url, parameters)
+
+        try:
+            response = urllib.request.urlopen(req)
+            response_body = response.read().decode("utf-8")
+            data = json.loads(response_body)
+            return data
+        except urllib.error.HTTPError as e:
+            print('The server couldn\'t fulfill the request.')
+            print('Error code: ', e.code)
+        except urllib.error.URLError as e:
+            print('We failed to reach a server.')
+            print('Reason: ', e.reason)
+
+    def count_product_numb(self, brand):
+        data = self.get_product_from_api(brand)
+        healthy_product = 0
+        for product in data["products"]:
+            try:
+                if product["nutrition_grade_fr"] == "a":
+                    healthy_product += 1
+            except:
+                pass
+        
+        return healthy_product
+
+if __name__ == "__main__":
+    api = OpenFoodFactsAPI()
+    healthy_product = api.count_product_numb("ferrero")
+    print(healthy_product)
---
+++ test_app.py
@@ -0,0 +1,45 @@
+from app import OpenFoodFactsAPI
+from unittest import TestCase
+from unittest.mock import patch
+
+class TestOpenFoodFactsAPI(TestCase):
+
+    @patch('app.OpenFoodFactsAPI')
+    def test_count_product_numb(self, mock_OpenFoodFactsAPI):
+
+        mock_OpenFoodFactsAPI.get_product_from_api.return_value = {
+            "count": 6,
+            "skip": 0,
+            "page_size": "150",
+            "page": 1,
+            "products": [
+                {
+                    "product_name_fr" : "Ferrero boite de 30",
+                    "nutrition_grade_fr": "a",
+                },
+                {
+                    "product_name_fr" : "Ferrero Light sans sucre et sans goût",
+                    "nutrition_grade_fr": "b",
+                },
+                {
+                    "product_name_fr" : "Ferrero Rocher",
+                    "nutrition_grade_fr": "e",
+                },
+                {
+                    "product_name_fr" : "Ferrero couscous",
+                    "nutrition_grade_fr": "a",
+                },
+                {
+                    "product_name_fr" : "Ferrero chocolat praliné",
+                    "nutrition_grade_fr": "d",
+                },
+                {
+                    "product_name_fr" : "Ferrero à la fraise",
+                    "nutrition_grade_fr": "c",
+                },
+                ]
+        }
+
+        #mock_OpenFoodFactsAPI.get_product_from_api.assert_called()
+        healthy_product = OpenFoodFactsAPI()
+        self.assertEqual(healthy_product.count_product_numb("ferrero"), 2)
